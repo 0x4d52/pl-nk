66bb0d870053d1d38296b2e9049211b0850e3a06
Martin Robinson <0x4d52@gmail.com>
2016-03-24 14:09:24 +0000

    Juce module PAEEngine tweak

--- plnk/juce_plnk.h
+++ plnk/juce_plnk.h
@@ -11,35 +11,13 @@
 #endif
 
 
-#if JUCE_USE_OGGVORBIS 
-#error You must disable JUCE_USE_OGGVORBIS in the Introjucer within the juce_audio_formats module
+#if JUCE_USE_OGGVORBIS && PLANK_OGGVORBIS
+#error You must disable JUCE_USE_OGGVORBIS in the Introjucer within the juce_audio_formats module if using PLANK_OGGVORBIS
 #endif
 
 #if PLONK_WARN_ABOUT_MISSING_PLONK_AUDIOHOST_JUCE_DEFINE && ! PLONK_AUDIOHOST_JUCE
 #warning You must add a preprocessor definition to the Introjucer project PLONK_AUDIOHOST_JUCE=1 to use the JUCE audio host
 #endif
-
-///** Config: PLANK_FFT_VDSP
-// Use vDSP in the Accerlerate framework on Mac OS X for the FFT processing.
-// */
-//#ifndef PLANK_FFT_VDSP
-//#define PLANK_FFT_VDSP 0
-//#endif
-//
-///** Config: PLANK_FFT_VDSP_FLIPIMAG
-// Flip thr polarity of the phase when using vDSP (this is to match the results of FFTReal).
-// */
-//#ifndef PLANK_FFT_VDSP_FLIPIMAG
-//#define PLANK_FFT_VDSP_FLIPIMAG 0
-//#endif
-//
-//
-///** Config: PLANK_VEC_VDSP
-// Use vDSP in the Accerlerate framework on Mac OS X to speed up vector maths operations.
-// */
-//#ifndef PLANK_VEC_VDSP
-//#define PLANK_VEC_VDSP 0
-//#endif
 
 
 #include "plnk.h"
